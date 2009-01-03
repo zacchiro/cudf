34d657353b3d962f3e0fa42deb1040214f84059a
Stefano Zacchiroli <zack@upsilon.cc>
2009-01-03 14:13:58 +0000

    preliminary type definitions

---
+++ cudf.ml
@@ -0,0 +1,35 @@
+
+open ExtLib
+
+type version = int
+
+type pkgname = string
+type vpkg = ([`Eq|`Neq|`Geq|`Gt|`Leq|`Lt] * version) option * pkgname
+type vpkglist = vpkg list
+type vpkgformula =
+    FTrue
+  | FPkg of vpkg
+  | FOr of vpkgformula list
+  | FAnd of vpkgformula list
+type veqpkg = ([`Eq] * version) option * pkgname
+type veqpkglist = veqpkg list
+
+type package = {
+  package : pkgname ;
+  version : version ;
+  depends : vpkgformula ;
+  conflicts : vpkglist ;
+  provides : veqpkglist ;
+  installed : bool ;
+  keep : [ `Keep_version | `Keep_package | `Keep_feature ] option ;
+}
+type request = {
+  install : vpkglist ;
+  remove : vpkglist ;
+  upgrade : vpkglist ;
+}
+type cudf = package list * request
+
+let lookup_package (pkgs, _req) id =
+  List.find (fun pkg -> (pkg.package, pkg.version) = id) pkgs
+
---
+++ cudf.mli
@@ -0,0 +1,50 @@
+
+(** {5 CUDF types} *)
+
+type version = int	(* required to be non 0 *)
+
+(** {6 CUDF spec. types} *)
+
+type pkgname = string
+type vpkg = ([`Eq|`Neq|`Geq|`Gt|`Leq|`Lt] * version) option * pkgname
+type vpkglist = vpkg list
+type vpkgformula = (* XXX does not enforce CNF, whereas the spec requires it *)
+    FTrue
+  | FPkg of vpkg
+  | FOr of vpkgformula list
+  | FAnd of vpkgformula list
+type veqpkg = ([`Eq] * version) option * pkgname
+type veqpkglist = veqpkg list
+
+(** {6 CUDF documents} *)
+
+(** Representation of a parsed package description item.
+
+    With this representation, optional properties have already been
+    expanded to their default values (if they have one). It is not
+    possible to know whether they were present or not in the CUDF
+    syntax. *)
+type package = {
+  package : pkgname ;
+  version : version ;
+  depends : vpkgformula ;	(* default : FTrue *)
+  conflicts : vpkglist ;	(* default : [] *)
+  provides : veqpkglist ;	(* default : [] *)
+  installed : bool ;		(* default : false *)
+  keep : [ `Keep_version | `Keep_package | `Keep_feature ] option ;
+}
+
+type request = {
+  install : vpkglist ;	(* default : [] *)
+  remove : vpkglist ;	(* default : [] *)
+  upgrade : vpkglist ;	(* default : [] *)
+}
+
+type cudf = package list * request
+
+(** {5 CUDF manipulation} *)
+
+(** lookup a specific package via a <name, version> key
+    @raise Not_found *)
+val lookup_package : cudf -> string * int -> package
+
