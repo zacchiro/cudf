02cafaf504bb3314c7fabade549942d4b11fcd9e
Stefano Zacchiroli <zack@upsilon.cc>
2009-01-04 16:18:55 +0000

    use full-fledged CUDF parser

--- cudf-check.ml
+++ cudf-check.ml
@@ -2,6 +2,10 @@
 open ExtLib
 
 let _ =
-  let p = Cudf_parser.from_in_channel stdin in
-  let stanza = Cudf_parser.parse_stanza p in
-     print_endline (dump stanza)
+  let ic =
+    try
+      open_in Sys.argv.(1)
+    with Invalid_argument _ -> stdin in
+  let p = Cudf_parser.from_in_channel ic in
+  let cudf = Cudf_parser.parse_cudf p in
+    print_endline (dump cudf)
