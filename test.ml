4e2cdd9faa4404816592f79022852c82a5171551
Stefano Zacchiroli <zack@upsilon.cc>
2009-01-05 08:06:16 +0000

    distinguish between CUDF parsing tests and universe parsing test (the latter not having a problem description item)

--- test.ml
+++ test.ml
@@ -22,25 +22,33 @@ open Printf
 let cudf_test_path name = sprintf "./tests/%s.cudf" name
 
 let good_cudfs = [	(* CUDF whose parsing must suceed *)
-  "conflict-comma-sep" ;
 ]
-
 let bad_cudfs = [	(* CUDF whose parsing must fail *)
 ]
+let good_pkgs = [	(* universes whose parsing must suceed *)
+  "conflict-comma-sep" ;
+]
+let bad_pkgs = [	(* universes whose parsing must fail *)
+]
+
+(** {6 OUnit helpers}
+    i.e., missing stuff which should better be integrated into OUnit *)
 
 let assert_no_exn f = assert_equal true (try f () ; true with _ -> false)
 
 let assert_raises' ?(cmp = (=)) ~exn f =
   assert_equal true (try f () ; false with exn' -> cmp exn exn')
 
-let good_parse fname = TestCase (fun _ ->
+(** {6 Test builders} *)
+
+let good_parse ~parse_fun fname = TestCase (fun _ ->
   let ic = open_in fname in
   let p = Cudf_parser.from_in_channel ic in
-    assert_no_exn (fun () -> Cudf_parser.parse_cudf p);
+    assert_no_exn (fun () -> parse_fun p);
     Cudf_parser.close p;
     close_in ic)
 
-let bad_parse fname = TestCase (fun _ ->
+let bad_parse ~parse_fun fname = TestCase (fun _ ->
   let ic = open_in fname in
   let p = Cudf_parser.from_in_channel ic in
     assert_raises'
@@ -49,22 +57,44 @@ let bad_parse fname = TestCase (fun _ ->
 		| Cudf_parser.Parse_error _, Cudf_parser.Parse_error _ -> true
 		| _ -> false)
       ~exn:(Cudf_parser.Parse_error (0, ""))
-      (fun () -> Cudf_parser.parse_cudf p);
+      (fun () -> parse_fun p);
     Cudf_parser.close p;
     close_in ic)
 
-let good_parse_suite =
-  "parsing of good CUDFs" >:::
-    List.map (fun n -> n >: good_parse (cudf_test_path n)) good_cudfs
+(** {6 Test suites} *)
 
-let bad_parse_suite =
-  "parsing of bad CUDFs" >:::
-    List.map (fun n -> n >: bad_parse (cudf_test_path n)) bad_cudfs
+let good_cudf_parse_suite =
+  "parsing of good CUDFs" >::: List.map
+      (fun n -> n >: good_parse ~parse_fun:Cudf_parser.parse_cudf
+	 (cudf_test_path n))
+      good_cudfs
+
+let bad_cudf_parse_suite =
+  "parsing of bad CUDFs" >::: List.map
+      (fun n -> n >: bad_parse ~parse_fun:Cudf_parser.parse_cudf
+	 (cudf_test_path n))
+      bad_cudfs
+
+let good_pkgs_parse_suite =
+  "parsing of good package universes" >::: List.map
+      (fun n -> n >: good_parse ~parse_fun:Cudf_parser.parse_packages
+	 (cudf_test_path n))
+      good_pkgs
+
+let bad_pkgs_parse_suite =
+  "parsing of good package universes" >::: List.map
+      (fun n -> n >: bad_parse ~parse_fun:Cudf_parser.parse_packages
+	 (cudf_test_path n))
+      bad_pkgs
+
+(** {6 Assemble and run tests} *)
 
 let all =
   "all tests" >::: [
-    good_parse_suite ;
-    bad_parse_suite
+    good_cudf_parse_suite ;
+    bad_cudf_parse_suite ;
+    good_pkgs_parse_suite ;
+    bad_pkgs_parse_suite ;
   ]
 
 let _ = run_test_tt_main all
