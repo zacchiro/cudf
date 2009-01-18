4b7b118e171260aed6e2bd551c94e1d4ab5ce087
Stefano Zacchiroli <zack@upsilon.cc>
2009-01-18 06:37:59 +0000

    make test do something (print package names)

--- c-lib/test.c
+++ c-lib/test.c
@@ -5,9 +5,19 @@
 
 int main(int argc, char **argv)
 {
+  cudf_doc doc;
+  int i;
+
   caml_startup(argv);
   if (argc < 2) {
     printf("Usage: %s CUDF_FILE\n", argv[0]);
     exit(2);
+  }
+
+  doc = cudf_parse_from_file(argv[1]);
+  printf("Universe size: %d\n", doc.length);
+  printf("Has request: %s\n", doc.has_request ? "yes" : "no");
+  for (i = 0; i < doc.length; i++) {
+    printf("  Package: %s\n", PKG_NAME(doc.packages[i]));
   }
 }
