f789206d31df58d1a97256cfc160a4b435575c4b
Stefano Zacchiroli <zack@upsilon.cc>
2011-11-28 22:18:09 +0100

    expose generic higher order printers in Cudf_printer
    That allows printing on arbitrary output backend, even when they are not easily
    supported by ExtLib.IO. Also, it allows to provide a different implementation
    of property pretty printing than the built in one.
    
    (Closes: #343)

--- cudf_printer.mli
+++ cudf_printer.mli
@@ -48,3 +48,62 @@ val pp_io_request : 'a IO.output -> request -> unit
 val pp_io_packages : 'a IO.output -> package list -> unit
 val pp_io_universe : 'a IO.output -> universe -> unit
 
+
+(** {6 Generic, higher-order pretty printers}
+
+    Usually, you shouldn't need those and you should be well served by the
+    above printers.
+
+    To bootstrap usage of the generic printers, you'll need to provide a
+    pp_property argument --- that takes a property as a pair of name/value
+    strings and print them on a generic output --- and then proceed composing
+    generic printers together.
+*)
+
+val pp_package_gen :
+  pp_property:('out -> string * string -> unit) ->
+    'out -> Cudf.package -> unit
+
+val pp_request_gen :
+  pp_property:('out -> string * string -> unit) ->
+    'out -> Cudf.request -> unit
+
+val pp_preamble_gen :
+  pp_property:('out -> string * string -> unit) ->
+    'out -> Cudf.preamble -> unit
+
+val pp_universe_gen :
+  pp_package:('out -> Cudf.package -> unit) ->
+  pp_sep:('out -> unit) ->
+    'out -> Cudf.universe -> unit
+
+val pp_packages_gen :
+  pp_package:('out -> Cudf.package -> unit) ->
+  pp_sep:('out -> unit) ->
+    'out -> Cudf.package list -> unit
+
+val pp_cudf_gen :
+  pp_preamble:('out -> Cudf.preamble -> unit) ->
+  pp_universe:('out -> Cudf.universe -> unit) ->
+  pp_request:('out -> Cudf.request -> unit) ->
+  pp_sep:('out -> unit) ->
+    'out -> Cudf.cudf -> unit
+
+val pp_doc_gen :
+  pp_preamble:('out -> Cudf.preamble -> unit) ->
+  pp_packages:('out -> Cudf.package list -> unit) ->
+  pp_request:('out -> Cudf.request -> unit) ->
+  pp_sep:('out -> unit) ->
+    'out -> Cudf.cudf_doc -> unit
+
+val pp_solution_gen :
+  pp_preamble:('out -> Cudf.preamble -> unit) ->
+  pp_universe:('out -> Cudf.universe -> unit) ->
+  pp_sep:('out -> unit) ->
+    'out -> Cudf.solution -> unit
+
+val pp_item_gen :
+  pp_package:('out -> Cudf.package -> unit) ->
+  pp_request:('out -> Cudf.request -> unit) ->
+  pp_preamble:('out -> Cudf.preamble -> unit) ->
+    'out -> Cudf.cudf_item -> unit
